91fb24e1c6b16d4745ccb7ef421e25263572340f
David Lee <dyl20b@my.fsu.edu>
2022-02-27 12:02:38 -0500

    Create Initial Unity Program

---
+++ SuperLizard/Assets/LizardScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LizardScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            rb.MovePosition(rb.position + Vector2.right);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            rb.MovePosition(rb.position + Vector2.left);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.MovePosition(rb.position + Vector2.up);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            rb.MovePosition(rb.position + Vector2.down);
+
+    }
+}
